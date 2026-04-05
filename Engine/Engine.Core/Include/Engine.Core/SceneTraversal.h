7d2ea406be445af92ac002db8eb2f19c8dbb833d
DvornikovArtem <mysteryrabatt@gmail.com>
2026-04-05 14:16:36 +0300

    Add scene traversal helpers

---
+++ Engine/Engine.Core/Include/Engine.Core/SceneTraversal.h
@@ -0,0 +1,36 @@
+﻿// SceneTraversal.h
+
+#pragma once
+
+#include <Engine.Core/AssetHandles.h>
+#include <Engine.Core/Types/SceneTypes.h>
+
+#include <directxtk/SimpleMath.h>
+
+namespace SimpleMath = DirectX::SimpleMath;
+
+namespace Engine::Core
+{
+	struct TraversedSceneNode
+	{
+		std::uint32_t NodeIndex = 0;
+		std::int32_t ParentIndex = -1;
+		SimpleMath::Matrix LocalTransform = SimpleMath::Matrix::Identity;
+		SimpleMath::Matrix WorldTransform = SimpleMath::Matrix::Identity;
+	};
+
+	struct SceneRenderable
+	{
+		std::uint32_t NodeIndex = 0;
+		MeshHandle Mesh;
+		SimpleMath::Matrix WorldTransform = SimpleMath::Matrix::Identity;
+	};
+
+	class SceneTraversal
+	{
+	public:
+		[[nodiscard]] static SimpleMath::Matrix BuildLocalTransform(const SceneNode& node);
+		[[nodiscard]] static std::vector<TraversedSceneNode> BuildNodeTransforms(const SceneAsset& scene);
+		[[nodiscard]] static std::vector<SceneRenderable> BuildRenderables(const SceneAsset& scene);
+	};
+}
---
+++ Engine/Engine.Core/src/SceneTraversal.cpp
@@ -0,0 +1,96 @@
+﻿// SceneTraversal.cpp
+
+#include <Engine.Core/SceneTraversal.h>
+
+namespace
+{
+	void TraverseSceneNode(
+		const Engine::Core::SceneAsset& scene,
+		const std::uint32_t nodeIndex,
+		const SimpleMath::Matrix& parentWorldTransform,
+		std::vector<Engine::Core::TraversedSceneNode>& traversedNodes,
+		std::vector<Engine::Core::SceneRenderable>* renderables)
+	{
+		const Engine::Core::SceneNode& node = scene.GetNode(nodeIndex);
+
+		const SimpleMath::Matrix localTransform = Engine::Core::SceneTraversal::BuildLocalTransform(node);
+		const SimpleMath::Matrix worldTransform = localTransform * parentWorldTransform;
+
+		Engine::Core::TraversedSceneNode traversedNode;
+		traversedNode.NodeIndex = nodeIndex;
+		traversedNode.ParentIndex = node.ParentIndex;
+		traversedNode.LocalTransform = localTransform;
+		traversedNode.WorldTransform = worldTransform;
+
+		traversedNodes[nodeIndex] = traversedNode;
+
+		if (renderables != nullptr)
+		{
+			for (Engine::Core::MeshHandle meshHandle : node.Meshes)
+			{
+				if (!meshHandle.IsValid())
+				{
+					continue;
+				}
+
+				Engine::Core::SceneRenderable renderable;
+				renderable.NodeIndex = nodeIndex;
+				renderable.Mesh = meshHandle;
+				renderable.WorldTransform = worldTransform;
+
+				renderables->push_back(renderable);
+			}
+		}
+
+		for (std::uint32_t childIndex : node.ChildrenIndices)
+		{
+			TraverseSceneNode(scene, childIndex, worldTransform, traversedNodes, renderables);
+		}
+	}
+}
+
+namespace Engine::Core
+{
+	SimpleMath::Matrix SceneTraversal::BuildLocalTransform(const SceneNode& node)
+	{
+		return
+			SimpleMath::Matrix::CreateScale(node.LocalScale) *
+			SimpleMath::Matrix::CreateFromQuaternion(node.LocalRotation) *
+			SimpleMath::Matrix::CreateTranslation(node.LocalTranslation);
+	}
+
+	std::vector<TraversedSceneNode> SceneTraversal::BuildNodeTransforms(const SceneAsset& scene)
+	{
+		std::vector<TraversedSceneNode> traversedNodes(scene.GetNodeCount());
+
+		for (std::uint32_t rootNodeIndex : scene.GetRootNodeIndices())
+		{
+			TraverseSceneNode(
+				scene,
+				rootNodeIndex,
+				SimpleMath::Matrix::Identity,
+				traversedNodes,
+				nullptr);
+		}
+
+		return traversedNodes;
+	}
+
+	std::vector<SceneRenderable> SceneTraversal::BuildRenderables(const SceneAsset& scene)
+	{
+		std::vector<TraversedSceneNode> traversedNodes(scene.GetNodeCount());
+		std::vector<SceneRenderable> renderables;
+
+		for (std::uint32_t rootNodeIndex : scene.GetRootNodeIndices())
+		{
+			TraverseSceneNode(
+				scene,
+				rootNodeIndex,
+				SimpleMath::Matrix::Identity,
+				traversedNodes,
+				&renderables);
+		}
+
+		return renderables;
+	}
+}
