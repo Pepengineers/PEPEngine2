d595849f63bfd2b24d555923162930b439f39512
Andrey Suvorov <andryusha.suvorov.05@mail.ru>
2026-05-30 00:55:31 +0300

    its not working now

--- Apps/App.Base/Include/App.Base/Entity.h
+++ Apps/App.Base/Include/App.Base/Entity.h
@@ -1,5 +1,10 @@
 #pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <limits>
+
 using Entity = uint32_t;
 
 static constexpr Entity InvalidEntity = 0;
-static constexpr size_t InvalidIndex = std::numeric_limits<size_t>::max();
+static constexpr size_t InvalidIndex = (std::numeric_limits<size_t>::max)();
