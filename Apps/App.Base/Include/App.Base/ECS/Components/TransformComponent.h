ba1ade7d24555337ae8919b62330d0a76f0f27b0
Andrey Suvorov <andryusha.suvorov.05@mail.ru>
2026-06-28 15:36:16 +0300

    post-merge fix

--- Apps/App.Base/Include/App.Base/ECS/Components/TransformComponent.h
+++ Apps/App.Base/Include/App.Base/ECS/Components/TransformComponent.h
@@ -16,10 +16,6 @@ struct TransformComponent : ComponentTag
     bool DirtyFlag;
 
     TransformComponent(Vector3 location = Vector3(0.f, 0.f, 0.f), 
-        Vector3 rotation = Vector3(0.f, 0.f, 0.f), Vector3 scale = Vector3(1.f, 1.f, 1.f)) 
-        
-TransformComponent(
-    Vector3 location = Vector3(0.f, 0.f, 0.f),
     Vector3 rotation = Vector3(0.f, 0.f, 0.f),
     Vector3 scale = Vector3(1.f, 1.f, 1.f))
     : Location(location),
@@ -28,9 +24,9 @@ TransformComponent(
       Rotation(rotation),
       Scale(scale),
       _CBufferIndex(0)
-{
-}
+        {
+        }
 
-std::uint32_t _CBufferIndex;
-std::uint32_t _numFramesDirty;
+    std::uint32_t _CBufferIndex;
+    std::uint32_t _numFramesDirty;
 };
